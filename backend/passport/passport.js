2e898b0c420e3dc6fa6c2aae47eb19c86830839f
Ovez Khan <khanovez257@gmail.com>
2025-11-04 15:33:41 +0530

    Added some changes

--- backend/passport/passport.js
+++ backend/passport/passport.js
@@ -22,7 +22,7 @@ passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET ,
   // callbackURL: "/auth/google/callback"
-  callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:5000/auth/google/callback",
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || "https://imagesearch-4g8h.onrender.com/auth/google/callback",
 
 
 }, async (accessToken, refreshToken, profile, done) => {
@@ -38,7 +38,7 @@ passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID ,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
   // callbackURL: "/auth/github/callback"
-  callbackURL: process.env.GITHUB_CALLBACK_URL || "http://localhost:5000/auth/github/callback",
+  callbackURL: process.env.GITHUB_CALLBACK_URL || "https://imagesearch-4g8h.onrender.com/auth/github/callback",
 
 
 }, async (accessToken, refreshToken, profile, done) => {
